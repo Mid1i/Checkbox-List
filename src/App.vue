3f518d4f6df9c27abc6e3b7feec3d86aa4c142ed
Mid1i <michac.tchernov@yandex.ru>
2025-02-18 22:03:03 +0300

    fix: Выпадающий список вынесен в отдельный компонент

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
-	import CheckboxGroup from "@/components/CheckboxGroup.vue";
-	import { ITEMS } from "@/constants";
+	import CheckboxDropdown from "@/components/CheckboxDropdown.vue";
 </script>
 
 
 <template>
 	<div class="container">
-		<CheckboxGroup
-			title="Ваши любимые фрукты"
-			:items="ITEMS"
-		/>
+		<a href="#" class="link">Ссылка</a>
+		<CheckboxDropdown title="Ваши любимые продукты"/>
+		<button class="button">Нажми на меня</button>
 	</div>
 </template>
 
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+	.container {
+		align-items: flex-start;
+		display: flex;
+		flex-direction: column;
+		padding: 40px 100px;
+	}
+
+	.link {
+		color: #FFFFFF;
+
+		&:focus {
+			color: red;
+		}
+	}
+
+	.button {
+		color: #FFFFFF;
+
+		&:focus {
+			color: red;
+		}
+	}
+</style>
